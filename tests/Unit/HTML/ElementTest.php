08140d26b37a4bf1d8a0bf9051c6e8c989a16120
jjgrainger <github@jjgrainger.co.uk>
2022-05-04 15:48:51 +0100

    add element tests

--- tests/Unit/HTML/ElementTest.php
+++ tests/Unit/HTML/ElementTest.php
@@ -12,4 +12,20 @@ class ElementTest extends TestCase
 
         $this->assertInstanceOf(Element::class, $element);
     }
+
+    public function test_element_can_set_tag()
+    {
+        $element = new Element();
+
+        $element->setTag('a');
+
+        $this->assertEquals($element->getTag(), 'a');
+    }
+
+    public function test_element_can_render()
+    {
+        $element = new Element();
+
+        $this->assertEquals($element->render(), '<div></div>');
+    }
 }

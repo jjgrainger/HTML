690d36c6f47e1f266804a3db60f5d63915bc79bb
jjgrainger <github@jjgrainger.co.uk>
2022-05-18 17:38:25 +0100

    add additional render tests

--- tests/Unit/HTML/ElementTest.php
+++ tests/Unit/HTML/ElementTest.php
@@ -28,4 +28,35 @@ class ElementTest extends TestCase
 
         $this->assertEquals($element->render(), '<div></div>');
     }
+
+    public function test_element_can_render_with_attributes()
+    {
+        $element = new Element();
+
+        $element->setAttributes([
+            'id' => 'main',
+            'class' => 'content',
+        ]);
+
+        $this->assertEquals($element->render(), '<div id="main" class="content"></div>');
+    }
+
+    public function test_element_can_render_with_content()
+    {
+        $element = new Element('p');
+
+        $element->setContent('Hello World!');
+
+        $this->assertEquals($element->render(), '<p>Hello World!</p>');
+    }
+
+    public function test_element_can_render_with_children()
+    {
+        $element = new Element('ol');
+
+        $element->addChild((new Element('li'))->setContent('List item 1'));
+        $element->addChild((new Element('li'))->setContent('List item 2'));
+
+        $this->assertEquals($element->render(), '<ol><li>List item 1</li><li>List item 2</li></ol>');
+    }
 }
